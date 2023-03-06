1c7f91e8a4558ba8508c8f067d7178cfb1683307
Fatma Çetin <granze_fatma88@hotmail.com>
2023-03-06 21:47:11 +0300

    Room classında düzenleme yapıldı

--- Project.ENTITIES/Models/Room.cs
+++ Project.ENTITIES/Models/Room.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project.ENTITIES.Models
 {
-    public class Room:BaseEntity,IRoomType
+    public class Room:BaseEntity
     {// oda class'ını açtım
         public string RoomNumber { get; set; }// oda numarası
+        public string StandartRoom { get; set; }
+        public decimal StandartPrice { get; set; }
+        public string SingleRoom { get; set; }
+        public decimal SinglePrice { get; set; }
+        public  string  SuitRoom { get; set; }
+        public decimal SuitPrice { get; set; }
         //relational
         public virtual List<RoomReservation> Rooms { get; set; }// oda rezervasyon çoka çok ilişkide listi burada
         public virtual List<RoomHousekeeper> Housekeepers { get; set; }// oda temizlikçi çoka çok ilişkide listi burada
         public virtual List<RoomServiceDetail> RoomServiceDetails { get; set; }// yemek servisi için yapılmış bir list
-        public string StandartRoom { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string SingleRoom { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string SuitRoom { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+      
     }
 }
