1f1cc84bfac72c2f37175c85900382a9c3f49806
SmA <121155503+smayvaci@users.noreply.github.com>
2023-03-09 11:56:43 +0300

    Project.DAL - yeniden düzenlendi

--- Project.DAL/ContextClasses/MyContext.cs
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -18,29 +18,32 @@ namespace Project.DAL.ContextClasses
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Configurations.Add(new CompanyCustomerMap());
-            modelBuilder.Configurations.Add(new CompanyCustomerGuestMap());
-            modelBuilder.Configurations.Add(new EmployeeMap());
-            modelBuilder.Configurations.Add(new EmployeeProfileMap());
+            modelBuilder.Configurations.Add(new CustomerMap());
+            modelBuilder.Configurations.Add(new CustomerGuestMap());
+            modelBuilder.Configurations.Add(new ReceptionMap());
+            modelBuilder.Configurations.Add(new ReceptionProfileMap());
             modelBuilder.Configurations.Add(new GuestMap());
             modelBuilder.Configurations.Add(new OrderMap());
             modelBuilder.Configurations.Add(new ReservationMap());
             modelBuilder.Configurations.Add(new RoomMap());
             modelBuilder.Configurations.Add(new RoomReservationMap());
             modelBuilder.Configurations.Add(new RoomTypeMap());
-
+            modelBuilder.Configurations.Add(new HouseKeeperMap());
+            modelBuilder.Configurations.Add(new RoomServiceMap());
         }
 
-        public DbSet<Customer> CompanyCustomer { get; set; }
-        public DbSet<CustomerGuest> CompanyCustomerGuests { get; set; }
-        public DbSet<Reception> Employees { get; set; }
-        public DbSet<ReceptionProfile> EmployeeProfiles { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerGuest> CustomerGuests { get; set; }
+        public DbSet<Reception> Receptions { get; set; }
+        public DbSet<ReceptionProfile> ReceptionProfiles { get; set; }
         public DbSet<Guest> Guests { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<RoomReservation> RoomReservations { get; set; }
         public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<RoomService> RoomsServices { get; set;}
+        public DbSet<HouseKeeper> HouseKeepers { get; set; }
 
 
 
